72464604a3a86d3d404775c1173ecdccda629474
VitorLK <vitor.lll@hotmail.com>
2020-06-05 11:22:39 -0300

    upload de arquivos e validacao de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,38 @@
 import express, { response } from 'express';
+import { celebrate, Joi } from 'celebrate';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
+const upload = multer(multerConfig);
 const routes = express.Router();
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create);
+routes.post(
+    '/points',
+     upload.single('image'),
+      celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+);
 
 routes.get('/points', pointsController.index);
 
